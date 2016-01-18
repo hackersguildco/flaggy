555071fde3e6af3d2258c0c4a5fa83322a9a21a4
mrkaspa <michel.ingesoft@gmail.com>
2016-01-18 08:32:49 -0500

    validate file exists

--- web/lib/process_photo.ex
+++ web/lib/process_photo.ex
@@ -42,6 +42,7 @@ defmodule Flaggy.ProcessPhoto do
     new_file_url = "#{@tmp_dir}#{user.fb_id}_new.jpeg"
     {size, _} = System.cmd("convert", ~w(-ping -format '%wx%h^' #{file_url} info:-))
     System.cmd("convert", ~w(#{file_url} #{@flag_path} -compose softlight -resize #{String.replace(size, "'", "")} -gravity center -composite -quality 100 demo.jpg))
+    File.read!(new_file_url)
     {user, new_file_url}
   end
 
